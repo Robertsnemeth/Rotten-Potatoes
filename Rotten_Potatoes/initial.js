4e713e91d4054674712dbc6b7bcb56ef80927efb
Robertsnemeth <104597057+Robertsnemeth@users.noreply.github.com>
2022-12-02 11:59:35 -0800

    inital build commit

--- Rotten_Potatoes/initial.js
+++
@@ -1,9 +0,0 @@
-const arr = [ 1,2,3,4,5,6,7,8,9,10 ]
-
-const sumOfArr = (arr) => {
-    const newArr = arr.reduce((num1, num2) => 
-        num1 + num2);
-    return newArr
-};
-
-console.log(sumOfArr(arr));
