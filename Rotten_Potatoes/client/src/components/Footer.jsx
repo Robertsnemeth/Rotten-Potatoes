b139a22a60008748dc6d532e429269db47c2b298
Robertsnemeth <104597057+Robertsnemeth@users.noreply.github.com>
2022-12-15 04:30:38 -0800

    target blank added to href

--- Rotten_Potatoes/client/src/components/Footer.jsx
+++ Rotten_Potatoes/client/src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 h-[100px] text-white flex items-center relative bottom-0">
         <div className='flex gap-2'>
-            <a href="https://github.com/Robertsnemeth/Rotten-Potatoes"><AiFillGithub size="30px"/></a>
-            <a href="https://www.linkedin.com/in/robert-s-nemeth/"><AiFillLinkedin size="30px"/></a>
+            <a href="https://github.com/Robertsnemeth/Rotten-Potatoes" target="_blank"><AiFillGithub size="30px"/></a>
+            <a href="https://www.linkedin.com/in/robert-s-nemeth/" target="_blank"><AiFillLinkedin size="30px"/></a>
         </div>
     </footer>
   )
