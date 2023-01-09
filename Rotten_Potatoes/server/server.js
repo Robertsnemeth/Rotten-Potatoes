1d03c0f4d80c056171adad9caf07b5e78c38097b
Robertsnemeth <104597057+Robertsnemeth@users.noreply.github.com>
2023-01-09 01:38:01 -0800

    origin env variable added for server.js

--- Rotten_Potatoes/server/server.js
+++ Rotten_Potatoes/server/server.js
@@ -3,11 +3,12 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const PORT = parseInt(process.env.SERVER_PORT);
+const ORIGIN = process.env.ORIGIN;
 const cookieParser = require('cookie-parser');
 
 app.use(express.json(), express.urlencoded({extensions: true}));
 app.use(cookieParser());
-app.use(cors({credentials: true, origin: 'http://127.0.0.1:5173'}));
+app.use(cors({credentials: true, origin: `${ORIGIN}`}));
 
 require("./routes/user.routes")(app);
 require("./routes/movie_watchlist.routes")(app);
